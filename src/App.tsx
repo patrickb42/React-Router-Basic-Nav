b100c21582c475349c8897bfdc82c72dc2577fc6
Patrick Baldwin <patrickbaldwin614@gmail.com>
2019-07-22 16:05:57 -0400

    wrapped App in Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 const App = () => (
-  <div>
-    <Navigation />
-  </div>
+  <Router>
+    <div>
+      <Navigation />
+    </div>
+  </Router>
 );
 
 export default App;
