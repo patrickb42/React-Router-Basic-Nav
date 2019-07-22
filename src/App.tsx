3e749b19916e539ce304030229be47fae24e8064
Patrick Baldwin <patrickbaldwin614@gmail.com>
2019-07-22 16:45:13 -0400

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 const App = () => (
   <Router>
     <div>
       <Navigation />
+      <Route exact path="/" component={Home}/>
+      <Route path="/about" component={About}/>
+      <Route path="/contact" component={Contact}/>
     </div>
   </Router>
 );
